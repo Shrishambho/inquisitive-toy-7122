7e29f87abbed28fd199d06a203200efcb30d3b87
shrishambho <shrikhade9096@gmail.com>
2023-05-05 22:24:05 +0530

    day-2

---
+++ Green_Leaf_Nursery/src/main/java/com/masai/Entity/Customer.java
@@ -0,0 +1,9 @@
+package com.masai.Entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Customer {
+
+	
+}
---
+++ Green_Leaf_Nursery/src/main/java/com/masai/Exception/SomeThingWentWrongException.java
@@ -0,0 +1,7 @@
+package com.masai.Exception;
+
+public class SomeThingWentWrongException extends Exception {
+    public SomeThingWentWrongException(String Message) {
+    	super(Message);
+    }
+}
